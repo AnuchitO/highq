164dabfa89b8bbb17f7bb48634fb5e0b219252b2
AnuchitO <anuchit.prasertasng@gmail.com>
2022-01-21 21:37:05 +0700

    binary implementation

--- binary/binary.go
+++ binary/binary.go
@@ -1,10 +1,30 @@
 package binary
 
+import (
+	"errors"
+	"fmt"
+	"strconv"
+)
+
 /* sParseBinary
 input: a string of binary digits
 	- "11", "1001", "11010", "10001101000"
 output: the decimal equivalent of the binary digits or an error
 */
 func ParseBinary(bin string) (int, error) {
-	return 0, nil
+	dec := 0
+	for _, v := range bin {
+		n, ok := strconv.Atoi(string(v))
+		switch {
+		case ok != nil:
+			msg := fmt.Sprintf("\"%c\" is not a vaid digit", v)
+			return 0, errors.New(msg)
+		case n < 0 || 1 < n:
+			msg := fmt.Sprintf("\"%d\" is not a binary digit", n)
+			return 0, errors.New(msg)
+		default:
+			dec = dec*2 + n
+		}
+	}
+	return dec, nil
 }
