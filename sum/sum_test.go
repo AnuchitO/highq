eb8638fa64fda313090e0b601d40086b2bd9bc5d
AnuchitO <anuchit.prasertasng@gmail.com>
2022-01-21 20:29:36 +0700

    test variadic func

--- sum/sum_test.go
+++ sum/sum_test.go
@@ -8,3 +8,10 @@ func TestSum(t *testing.T) {
 		t.Error("Expected", 5, "Got", r)
 	}
 }
+
+func TestSumAll(t *testing.T) {
+	r := Sum([]int{2, 3, 3}...)
+	if r != 8 {
+		t.Error("Expected", 8, "Got", r)
+	}
+}
