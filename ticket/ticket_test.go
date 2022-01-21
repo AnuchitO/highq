83a39bbc7954b643d2495d7810df792722946ab2
AnuchitO <anuchit.prasertasng@gmail.com>
2022-01-21 21:36:56 +0700

    fix test ticket

--- ticket/ticket_test.go
+++ ticket/ticket_test.go
@@ -3,9 +3,11 @@ package ticket
 import "testing"
 
 func TestPriceFree(t *testing.T) {
+	want := 12.0
+
 	p := Price(Order{Age: 4})
-	if p != 0 {
-		t.Error("Expected", 0, "Got", p)
+	if p != want {
+		t.Error("Expected", want, "Got", p)
 	}
 }
 
