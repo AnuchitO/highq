d9b59f918356adda14fadbc9528d6e3e500b3c26
AnuchitO <anuchit.prasertasng@gmail.com>
2022-01-21 21:24:02 +0700

    test table

--- ticket/ticket_test.go
+++ ticket/ticket_test.go
@@ -8,3 +8,25 @@ func TestPriceFree(t *testing.T) {
 		t.Error("Expected", 0, "Got", p)
 	}
 }
+
+var testCases = []struct {
+	order Order
+	want  float64
+}{
+	{Order{Age: 0}, 0},
+	{Order{Age: 4}, 12},
+	{Order{Age: 12}, 12},
+	{Order{Age: 13}, 25},
+	{Order{Age: 65}, 25},
+	{Order{Age: 66}, 5},
+}
+
+func TestPriceAll(t *testing.T) {
+	for _, tt := range testCases {
+		got := Price(tt.order)
+
+		if got != tt.want {
+			t.Fatalf("actual %v, expected %v", got, tt.want)
+		}
+	}
+}
