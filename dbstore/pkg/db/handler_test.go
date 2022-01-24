fc12aebbaa62787313efb821814f4163515cf731
AnuchitO <anuchit.prasertasng@gmail.com>
2022-01-24 22:48:56 +0700

    dbstore: decouple db and file : inject service for Handler in test & use New setupFile

--- dbstore/pkg/db/handler_test.go
+++ dbstore/pkg/db/handler_test.go
@@ -11,9 +11,11 @@ import (
 
 func TestSingleHttpDelete(t *testing.T) {
 	// setup
-	testdb, teardown := setup(t)
+	f, teardown := setupFile(t)
 	defer teardown()
-	r := NewMainHandler(testdb)
+	database := New(f)
+	service := NewService(database)
+	r := NewMainHandler(service)
 	srv := httptest.NewServer(r)
 
 	// act
@@ -47,9 +49,11 @@ func TestSingleHttpDelete(t *testing.T) {
 
 func TestSingleHttpSetAndGet(t *testing.T) {
 	// setup
-	testdb, teardown := setup(t)
+	f, teardown := setupFile(t)
 	defer teardown()
-	r := NewMainHandler(testdb)
+	database := New(f)
+	service := NewService(database)
+	r := NewMainHandler(service)
 	srv := httptest.NewServer(r)
 
 	// act
