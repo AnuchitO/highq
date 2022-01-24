63bd7475856c0817174e3200f04a7035ea480a03
AnuchitO <anuchit.prasertasng@gmail.com>
2022-01-24 22:25:31 +0700

    dbstore: use only file to read and write

--- dbstore/pkg/db/db.go
+++ dbstore/pkg/db/db.go
@@ -15,8 +15,7 @@ import (
 // DB type
 type DB struct {
 	filename  string
-	fileWrite io.WriteSeeker
-	fileRead  io.ReadSeeker
+	f         io.ReadWriteSeeker
 	offsetMap map[string]int64
 }
 
@@ -36,7 +35,7 @@ func (db *DB) pbAppend(entity *pb.Entity) (int64, error) {
 		return 0, fmt.Errorf("pb marshall error %v", err)
 	}
 	byteBuffer := writeBinaryBufferLength(entityBytes)
-	offset, err := db.fileWrite.Seek(0, 2)
+	offset, err := db.f.Seek(0, 2)
 	if err != nil {
 		return 0, fmt.Errorf("file seek error %v", err)
 	}
@@ -44,7 +43,7 @@ func (db *DB) pbAppend(entity *pb.Entity) (int64, error) {
 	if err != nil {
 		return 0, fmt.Errorf("error writing byte buffer %v", err)
 	}
-	_, err = db.fileWrite.Write(byteBuffer.Bytes())
+	_, err = db.f.Write(byteBuffer.Bytes())
 	if err != nil {
 		return 0, fmt.Errorf("entity size file write error %v", err)
 	}
@@ -81,7 +80,7 @@ func (db *DB) Get(key string) (*pb.Entity, error) {
 	if !ok {
 		return nil, nil
 	}
-	_, err := db.fileRead.Seek(offset, 0)
+	_, err := db.f.Seek(offset, 0)
 	if err != nil {
 		return nil, fmt.Errorf("file seek error %v", err)
 	}
@@ -102,7 +101,7 @@ func (db *DB) Get(key string) (*pb.Entity, error) {
 func (db *DB) readSize() (uint64, error) {
 	intsize := 8
 	byteBuffer := make([]byte, intsize)
-	_, err := db.fileRead.Read(byteBuffer)
+	_, err := db.f.Read(byteBuffer)
 	if err != nil {
 		return 0, err
 	}
@@ -119,7 +118,7 @@ func (db *DB) readSize() (uint64, error) {
 func (db *DB) Recover() error {
 	// start reading file at beginning
 	offset := int64(0)
-	_, err := db.fileRead.Seek(offset, 0)
+	_, err := db.f.Seek(offset, 0)
 	if err != nil {
 		return fmt.Errorf("file seek error %v", err)
 	}
@@ -147,7 +146,7 @@ func (db *DB) Recover() error {
 
 func (db *DB) readPbData(lengthOf uint64) (*pb.Entity, error) {
 	dataBuf := make([]byte, lengthOf)
-	_, err := db.fileRead.Read(dataBuf)
+	_, err := db.f.Read(dataBuf)
 	if err != nil {
 		return nil, err
 	}
@@ -163,15 +162,12 @@ func (db *DB) readPbData(lengthOf uint64) (*pb.Entity, error) {
 
 // NewDb return a new intialized Db
 func NewDb(filename string) *DB {
-	fileWrite, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY, 0644)
+	f, err := os.OpenFile(filename, os.O_CREATE|os.O_RDWR, 0644)
 	if err != nil {
 		log.Fatalf("error file opening for write")
 	}
-	fileRead, err := os.OpenFile(filename, os.O_RDONLY, 0644)
-	if err != nil {
-		log.Fatalf("error file opening for read")
-	}
+
 	offsetMap := make(map[string]int64)
-	db := &DB{filename: filename, fileWrite: fileWrite, fileRead: fileRead, offsetMap: offsetMap}
+	db := &DB{filename: filename, f: f, offsetMap: offsetMap}
 	return db
 }
