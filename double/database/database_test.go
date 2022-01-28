9de0b04e2d6b8289d756d36535f4091185be2e78
AnuchitO <anuchit.prasertasng@gmail.com>
2022-01-28 20:39:29 +0700

    double: WORKSHOP database : solution

--- double/database/database_test.go
+++ double/database/database_test.go
@@ -2,6 +2,20 @@ package database
 
 import "testing"
 
+type fakeDB struct {
+	Database
+}
+
+func (*fakeDB) Insert(collection string, data interface{}) error {
+	return nil
+}
+
 func TestInsert(t *testing.T) {
-	t.Fail()
+	mock := &fakeDB{}
+
+	err := Insert(mock, "product", `{}`)
+
+	if err != nil {
+		t.Error(err.Error())
+	}
 }
