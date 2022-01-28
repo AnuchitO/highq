be8a1006df65e477f069105b29f5582b53140cdc
AnuchitO <anuchit.prasertasng@gmail.com>
2022-01-28 20:14:28 +0700

    double: WORKSHOP : solution

--- double/sql/sql.go
+++ double/sql/sql.go
@@ -2,7 +2,11 @@ package sql
 
 import "database/sql"
 
-func execQuery(db *sql.DB, query string, args ...interface{}) (int64, error) {
+type DB interface {
+	Exec(query string, args ...interface{}) (sql.Result, error)
+}
+
+func execQuery(db DB, query string, args ...interface{}) (int64, error) {
 	res, err := db.Exec(query, args...)
 	if err != nil {
 		return 0, err
--- double/sql/sql_test.go
+++ double/sql/sql_test.go
@@ -1,7 +1,41 @@
 package sql
 
-import "testing"
+import (
+	"database/sql"
+	"testing"
+)
+
+type mockDB struct {
+	query        string
+	lastInsertID int64
+	rowsAffected int64
+}
+
+func (m *mockDB) LastInsertId() (int64, error) {
+	return m.lastInsertID, nil
+}
+func (m *mockDB) RowsAffected() (int64, error) {
+	return m.rowsAffected, nil
+}
+
+func (m *mockDB) Exec(query string, args ...interface{}) (sql.Result, error) {
+	m.query = query
+	return m, nil
+}
 
 func TestExecQuery(t *testing.T) {
-	t.Fail()
+	mock := &mockDB{
+		rowsAffected: 32,
+	}
+
+	r, _ := execQuery(mock, "SELECT * FROM sql")
+
+	if mock.query != "SELECT * FROM sql" {
+		t.Error("should have been call db.Exec with query but it not.")
+	}
+
+	if r != 32 {
+		t.Errorf("should return row effect %d but it got %d.", 32, r)
+	}
+
 }
